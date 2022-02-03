1589ea05897025c7a3f036cfc69ddf05a1f7600d
Sai Sarath <saisarathchandrap@gmail.com>
2022-02-03 05:54:55 +0530

    Better solution to Reverse a String

--- src/datastructures/arrays/ReverseAString.java
+++ src/datastructures/arrays/ReverseAString.java
@@ -1,23 +1,19 @@
 package datastructures.arrays;
 
 
-import java.util.Arrays;
-
 public class ReverseAString {
 
-    public String reverse(String input){
-        char[] inputArr = input.toCharArray();
-        final int inputLength = inputArr.length;
-        char[] reverseArr = new char[inputLength];
-        for (int i = 0; i < inputLength; i++) {
-            reverseArr[i] = inputArr[inputLength-i-1];
+    public StringBuilder reverse(String input){
+        StringBuilder reversedString = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversedString.append(input.charAt(i));
         }
-        return String.valueOf(reverseArr);
+        return reversedString;
     }
 
     public static void main(String[] args) {
         ReverseAString reverseAString = new ReverseAString();
-        final String reversedString = reverseAString.reverse("Sai");
+        final String reversedString = String.valueOf(reverseAString.reverse("Sai"));
         System.out.println(reversedString);
     }
 }
